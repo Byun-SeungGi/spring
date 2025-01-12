28782ac8f293fd70f79a047c56646788bce47e82
Byun <smk0494@daum.net>
2025-01-12 20:02:44 +0900

    250112 AppConfig 추가

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -2,7 +2,7 @@ package hello.core;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import hello.core.member.MemberSerivce;
+import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
@@ -11,12 +11,13 @@ import hello.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberSerivce memberSerivce = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
-        memberSerivce.join(member);
+        memberService.join(member);
 
         Order order = orderService.createOrder(memberId, "itemA", 10000);
 
